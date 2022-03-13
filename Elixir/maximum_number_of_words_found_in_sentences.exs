7d9c00730eeb80525119cbe24712bd20b35c7a22
meinbaumm <max413111@mail.ru>
2022-03-13 20:28:54 +0300

    small fix

--- Elixir/maximum_number_of_words_found_in_sentences.exs
+++ Elixir/maximum_number_of_words_found_in_sentences.exs
@@ -2,19 +2,19 @@
 # 2114. Maximum Number of Words Found in Sentences
 
 defmodule Solution do
-  @spec most_words_found(sentences :: [String.t]) :: integer
-    defp count_spaces(sentences) do
-      Enum.map(sentences, fn s ->
-        freq = String.graphemes(s) |> Enum.frequencies()
-        case Map.get(freq, " ") do
-          nil -> 0
-          _ -> Map.get(freq, " ")
-        end
-      end)
-      |> Enum.max()
-    end
+  defp count_spaces(sentences) do
+    Enum.map(sentences, fn s ->
+      freq = String.graphemes(s) |> Enum.frequencies()
+      case Map.get(freq, " ") do
+        nil -> 0
+        _ -> Map.get(freq, " ")
+      end
+    end)
+    |> Enum.max()
+  end
 
-    def most_words_found(sentences) do
-      count_spaces(sentences) + 1
-    end
+  @spec most_words_found(sentences :: [String.t]) :: integer
+  def most_words_found(sentences) do
+    count_spaces(sentences) + 1
+  end
 end
