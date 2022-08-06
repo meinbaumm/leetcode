2f1c207f13d5672e776dd325e3ae00c485f5f221
meinbaumm <max413111@mail.ru>
2022-08-06 21:06:06 +0700

    solution for task_1108

---
+++ rust/src/easy/task_1108.rs
@@ -0,0 +1,25 @@
+// 1108. Defanging an IP Address
+// https://leetcode.com/problems/defanging-an-ip-address/
+
+use crate::Solution;
+
+impl Solution {
+    pub fn defang_i_paddr(address: String) -> String {
+        address.replace(".", "[.]")
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_1() {
+        assert_eq!("1[.]1[.]1[.]1", Solution::defang_i_paddr("1.1.1.1".to_string()));
+    }
+
+    #[test]
+    fn test_2() {
+        assert_eq!("255[.]100[.]50[.]0", Solution::defang_i_paddr("255.100.50.0".to_string()));
+    }
+}
