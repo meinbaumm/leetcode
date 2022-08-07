4bf8f7b1aebc3522984ef694cf6268622b47ccb2
meinbaumm <max413111@mail.ru>
2022-08-07 20:33:16 +0700

    solution for task 771

---
+++ Elixir/test/task_771_test.exs
@@ -0,0 +1,14 @@
+defmodule Task771Test do
+  use ExUnit.Case
+  doctest Task771
+
+  test "capitalize the title" do
+    jewels1 = "aA"
+    stones1 = "aAAbbbb"
+    assert Task771.num_jewels_in_stones(jewels1, stones1) == 3
+
+    jewels2 = "z"
+    stones2 = "ZZ"
+    assert Task771.num_jewels_in_stones(jewels2, stones2) == 0
+  end
+end
